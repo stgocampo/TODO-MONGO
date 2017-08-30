3fc039906dfbf1758b3535608a1bbcc352d12f73
stgocampo <santi0227@gmail.com>
2017-08-30 16:16:30 -0500

    Agregando POST /users/login route

--- server/server.js
+++ server/server.js
@@ -119,7 +119,6 @@ app.get('/users', (req, res) => {
 app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
-  debugger;
   user.save().then(() => {
     return user.generateAuthToken();
   })
@@ -133,6 +132,18 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    })
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
 app.listen(port, () => {
